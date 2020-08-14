e23341b1a300c0bd450b0b9fcbd9416adeceb7b2
nickkhall <nick.hall@bluelogicteam.com>
2020-08-14 16:35:32 -0500

    renamed header include guard

--- src/headers/common.h
+++ src/headers/common.h
@@ -1,10 +1,10 @@
-#ifndef __EMP_MAN_RPC_COMMON__
-#define __EMP_MAN_RPC_COMMON__
+#ifndef __EMP_MAN_REST_COMMON__
+#define __EMP_MAN_REST_COMMON__
 
 #define MAX_RECV_SEND_BUFF_SIZE 2048
 
 #define SERVER_PORT 8778
 #define SERVER_IP   "127.0.0.1"
 
-#endif /* __EMP_MAN_RPC_COMMON__ */
+#endif /* __EMP_MAN_REST_COMMON__ */
 
