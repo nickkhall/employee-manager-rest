c9bdc9cf0d40c28e20c5a3af10f05af0da0d043d
nickkhall <nick.hall@bluelogicteam.com>
2020-08-17 10:39:23 -0500

    stable, working multiply rest functionality

--- main.c
+++ main.c
@@ -90,10 +90,15 @@ int multiply_rpc(int a, int b) {
 int main(int argc, char** argv) {
   int a = 20;
   int b = 10;
+  int c = 5;
 
+  printf("Calling `multiply_rpc` with a: %d, b: %d\n", a, b);
   int res = multiply_rpc(a, b);
-
-  printf("res = %d\n", res);
+  printf("CLIENT - RPC RESPONSE: res = %d\n", res);
+  
+  printf("Calling `multiply_rpc` with a: %d, b: %d\n", c, a);
+  int res_two = multiply_rpc(c, a);
+  printf("CLIENT - RPC RESPONSE: res_two = %d\n", res_two);
 
   return 0;
 }
