c2efa1273124dfb0f826019f8eaf5aeb65eff6bb
nickkhall <nick.hall@bluelogicteam.com>
2020-10-09 17:32:40 -0500

    finished generic list iterator fn ptr implementation to demonstrate get_emp_by_id

--- main.c
+++ main.c
@@ -7,7 +7,17 @@
 void print_employee(employee_t* item);
 
 void print_employee(employee_t* item) {
+  printf("id: %s\n", item->id);
   printf("first name: %s\n", item->first);
+  printf("last name: %s\n", item->last);
+  printf("email: %s\n", item->email);
+  printf("phone: %s\n", item->phone);
+  printf("address: %s\n", item->address);
+  printf("salary: %d\n", *item->salary);
+  printf("ethnicity: %s\n", item->ethnicity);
+  printf("title %s\n", item->title);
+  printf("gender: %s\n", item->gender);
+  printf("start: %ld\n", *item->start);
 }
 
 int main(int argc, char** argv) {
