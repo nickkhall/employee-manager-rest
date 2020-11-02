a24427c55c4e67239e7153f5803018311c098dac
nickkhall <nick.hall@bluelogicteam.com>
2020-11-02 14:59:35 -0600

    staborken, new server architecture poc

--- src/server.c
+++ src/server.c
@@ -11,6 +11,8 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <libexplain/bind.h>
+#include <string.h>
+#include <fcntl.h> // nonblocking socket
 
 #include <serialize.h>
 #include <sockets.h>
@@ -21,10 +23,15 @@
 
 int* server_init(void)
 {
-  // create main server socket
+  // create server socket
   int* server_socket = server_new_socket(REST_SERVER_PORT);
-  struct sockaddr_storage server_storage;
   struct sockaddr_in* server_addr = socklib_socket_build_sock_addr_in(server_socket, AF_INET, REST_SERVER_PORT);
+
+  // create client socket
+  int client_socket;
+  struct sockaddr_in client_addr;
+
+  int last_socket = *server_socket;
 
   // bind to REST port
   int binded = bind(*server_socket, (struct sockaddr*) &(*server_addr), sizeof(struct sockaddr));
@@ -33,9 +40,6 @@ int* server_init(void)
     exit(EXIT_FAILURE);
   }
   
-  struct sockaddr_in client_addr;
-  int addr_len = sizeof(struct sockaddr);
-
   // attempt to start listening on REST port
   int listen_status = listen(*server_socket, 40);
   if (listen_status < 0) {
@@ -43,7 +47,15 @@ int* server_init(void)
     exit(EXIT_FAILURE);
   }
 
-  pid_t pid[50];
+  int client_addr_len = sizeof(client_addr);
+  client_socket = accept(*server_socket, (struct sockaddr*) &client_addr, (socklen_t*) &client_addr_len);
+  if (client_socket == -1) {
+    perror("accept"); 
+  }
+
+  // change the sockets into non-blocking state
+  fcntl(last_socket, F_SETFL, O_NONBLOCK);
+  fcntl(client_socket, F_SETFL, O_NONBLOCK);
 
   // create and initialize send/recv buffers
   ser_buff_t** recv_buffer = (ser_buff_t**) malloc(MAX_RECV_BUFF_SIZE);
@@ -53,39 +65,36 @@ int* server_init(void)
   // reset recv buffer
   serlib_reset_buffer(*recv_buffer);  
 
-  if (listen_status == 0) {
-    // print running message to screen
-    printf("+------------------------------------------------------------------------+\n");
-    printf("  > Employee Manager REST - Server is now listening on %s:%d\n", REST_SERVER_HOST, REST_SERVER_PORT);
-    int i = 0;
+  int i, n;
+  while(1) {
+    for (i = *server_socket; i <= last_socket; i++) {
+      printf("Round number %d\n", i);
+      if (i == *server_socket) {
+        client_addr_len = sizeof(struct sockaddr_in);
 
-    while(1) {
-      addr_len = sizeof(server_storage);
-      *server_socket = accept(*server_socket, (struct sockaddr*) &client_addr, (socklen_t*) &addr_len);
+        if ((client_socket = accept(*server_socket, (struct sockaddr*) &client_addr, (socklen_t*) &client_addr_len)) == -1) {
+          perror("accept");
+        }
 
-      int pid_c = 0;
-      pid_c = fork();
+        printf("REST Server: received connection from %d\n", inet_ntoa(client_addr.sin_addr));
 
-      if (pid_c == 0) {
-        server_socket_new_thread((REST_SERVER_PORT + i),
-                                recv_buffer, send_buffer,
-                                (struct sockaddr*) &client_addr,
-                                (socklen_t*) &addr_len);
+        fcntl(client_socket, F_SETFL, O_NONBLOCK);
+        last_socket = client_socket;
       } else {
-        *(pid + (i++)) = pid_c;
-
-        if (i >= 49) {
-          i = 0;
-
-          while (i < 50) {
-            waitpid(*(pid + (i++)), NULL, 0);
+        n = recv(client_socket, &(*(*recv_buffer)->buffer), serlib_get_buffer_length(*recv_buffer), 0);
+        if (n < 1) {
+          perror("recv - non blocking \n");
+          printf("REST Server ERROR:: NON-BLOCKING - Round %d\nData Received - Bytes: %d\n", i, n);
+        } else {
+          *((*recv_buffer)->buffer + n) = '\0'; 
+          printf("Data from client:\n%s\n\n", (*recv_buffer)->buffer);
+          if (send(client_socket, "Hello from the server! :)\n", 26, 0) == -1) {
+            perror("send");
           }
-          
-          i = 0;
         }
       }
     }
-  }    
+  }   
 }
 
 int* server_new_socket(int port) {
@@ -132,7 +141,7 @@ void server_process_traffic(ser_buff_t** recv_buffer, ser_buff_t** send_buffer)
   switch (rest_ser_header->rpc_proc_id) {
     default:
       printf("default switch case called\n");
-      printf("PROCESS ID: %s\n", rest_ser_header->rpc_proc_id);
+      printf("PROCESS ID: %d\n", rest_ser_header->rpc_proc_id);
       break;
   }
 }
@@ -141,13 +150,13 @@ void server_socket_new_thread(int port,
                               ser_buff_t** recv_buffer,
                               ser_buff_t** send_buffer,
                               struct sockaddr* client_addr,
-                              socklen_t* addr_len)
+                              socklen_t* client_addr_len)
 { 
   // create new socket
-  int new_socket = server_new_socket(port);;
+  int* new_socket = server_new_socket(port);;
 
   // receive data
-  int len = recv(*socket, &(*(*recv_buffer)->buffer), serlib_get_buffer_length(*recv_buffer), 0);
+  int len = recv(*new_socket, &(*(*recv_buffer)->buffer), serlib_get_buffer_length(*recv_buffer), 0);
   
   // create and apply thread lock
   pthread_mutex_t lock;
@@ -167,7 +176,7 @@ void server_socket_new_thread(int port,
   pthread_mutex_unlock(&lock);
   
   // send the serialized result to client
-  len = send(new_socket, (*send_buffer)->buffer, serlib_get_buffer_length(*send_buffer), 0); 
+  len = send(*new_socket, (*send_buffer)->buffer, serlib_get_buffer_length(*send_buffer), 0); 
 
   // print sent bytes
   printf("Employee Manager REST - Sent %d bytes.\n", len);
@@ -176,6 +185,6 @@ void server_socket_new_thread(int port,
   serlib_reset_buffer(*send_buffer);
 
   // close socket
-  close(new_socket);
+  close(*new_socket);
 }
 
